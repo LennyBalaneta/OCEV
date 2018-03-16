5bed5bdeb045135e42756c20ee4f9c2d10927dc0
Marcos Balatka <m.balatka5@gmail.com>
2018-03-16 06:18:31 -0300

    AckleyFunc para codificacao binaria

--- IndividuoBin.py
+++ IndividuoBin.py
@@ -1,5 +1,6 @@
 from Individuo import *
 import numpy as np
+import math
 
 class IndividuoBin(Individuo):
     def __init__(self, tam, minB=-10, maxB=10):
@@ -23,13 +24,55 @@ class IndividuoBin(Individuo):
         return f
 
     def fitness(self):
-        bitsPnum = 5
-        nVars = int(len(self.cromossomo)/bitsPnum)
-        #print("bitsPnum->", bitsPnum, " | nVars->", nVars)
-        for i in range(nVars):
-            self.cromDecode(self.cromossomo, bitsPnum*i, bitsPnum*(i+1))
-        return -1
+        '''
+        var1 -> [0:13]
+        var2 -> [13:26]
+        '''
+        if len(self.cromossomo) != 26:
+            print("Tamanho do cromossomo incorreto para aplicar na funcao de ackley")
+            return -1
+        vars = [self.cromDecode(self.cromossomo, 0, 13), self.cromDecode(self.cromossomo, 13, 26)]
+        return self.ackleyFunc(vars)
 
     def cromDecode(self, c, ini, fin):
-        print("CromPart: ", self.cromossomo[ini:fin])
-        #self.cromossomo[bitsPnum*i:bitsPnum*(i+1)]
+        '''
+        Estrutura do numero binario:
+        [0:6] -> parte inteira
+        [6:13] -> parte decimal
+        '''
+        numTotal = c[ini:fin]
+        
+        pInt = numTotal[0:6]
+        pDec = numTotal[6:13]
+        
+        numInt = 0
+        for i in range(len(pInt)):
+            numInt += pInt[i] * (2**(len(pInt)-i-1))
+        numDec = 0
+        for i in range(len(pDec)):
+            numDec += pDec[i] * (2**(len(pDec)-i-1))
+        inBounds = self.ajuste(numInt + (numDec/100))
+        return self.ajuste(numInt + (numDec/100))
+        
+    def ajuste(self, num):
+        '''
+        numero recebido -> [0, 64.27]
+        bounds desejados -> [-30, 30]
+        '''
+        num -= 30
+        if num > 30:
+            num = 30
+        return num
+
+    def ackleyFunc(self, vars):
+        first_sum = 0.0
+        second_sum = 0.0
+        for v in range(len(vars)):
+            first_sum += vars[v] ** 2.0
+            second_sum += math.cos(2.0 * math.pi * vars[v])
+        n = float(len(vars))
+        return -20.0*math.exp(-0.2*math.sqrt(first_sum/n)) - math.exp(second_sum/n) + 20 + math.e
+        
+        
+
+        
--- IndividuoReal.py
+++ IndividuoReal.py
@@ -13,12 +13,13 @@ class IndividuoReal(Individuo):
         return np.random.RandomState().uniform(self.min_bound, self.max_bound, size=tamCrom)
         
     def fitness(self):
-        ##
+        return self.ackleyFunc(self.cromossomo)
+    
+    def ackleyFunc(self, vars):
         first_sum = 0.0
         second_sum = 0.0
-        for g in self.cromossomo:
-            first_sum += g ** 2.0
-            second_sum += math.cos(2.0 * math.pi * g)
-        n = float(len(self.cromossomo))
+        for v in range(len(vars)):
+            first_sum += vars[v] ** 2.0
+            second_sum += math.cos(2.0 * math.pi * vars[v])
+        n = float(len(vars))
         return -20.0*math.exp(-0.2*math.sqrt(first_sum/n)) - math.exp(second_sum/n) + 20 + math.e
-        ##
